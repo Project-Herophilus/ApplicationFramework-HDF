ff12e999a1f0cb187fad95ae0b9a9275d57c8fc7
balanscott <balanscott@live.com>
2022-10-22 00:22:34 -0500

    Updates:
    - Created notifications.js API and updated packages to support communications with Twilio and Email.

--- Node-Notifier/api/notifications.js
+++ Node-Notifier/api/notifications.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const twilio = require('twilio');
+const accountSid = process.env.twilioSID; // Your Account SID from www.twilio.com/console
+const authToken = process.env.twilioToken; // Your Auth Token from www.twilio.com/console
+const client = new twilio(accountSid, authToken);
+
 
 router.get("/notification-email", async(req, res) => {
 
 });
 
 router.get("/notification-sms", async(req, res) => {
+    client.messages
+        .create({
+            body: msg,
+            to: to, // Text this number
+            from: process.env.twilioPhoneNumber
+        })
+        .then((message) => console.log(message.sid)).done();
 
 });
 
