338f9170e9f4e8839ca194dde4e908a6c92c3f96
balanscott <balanscott@live.com>
2022-10-21 23:37:22 -0500

    Updates
    - Added notifier-email.js
    - renamed notifier to notifier-sms.js
    - Added nodemailer package

--- Node-Intelligent-DataRouter/notifier-sms.js
+++ Node-Intelligent-DataRouter/notifier-sms.js
@@ -1,12 +1,13 @@
 const twilio = require('twilio');
-const accountSid = process.env.twilioAccount; // Your Account SID from www.twilio.com/console
+const accountSid = process.env.twilioSID; // Your Account SID from www.twilio.com/console
 const authToken = process.env.twilioToken; // Your Auth Token from www.twilio.com/console
 const client = new twilio(accountSid, authToken);
 const args = process.argv.slice(2);
 //const appName="DataSynthesis";
 smsmessageDtl = args[0];
 smsTextNumber = args[1];
-
+// Details are configurable within
+// https://console.twilio.com/us1/console?frameUrl=/console
 function sendSMSMessage(msg, to) {
     client.messages
         .create({
@@ -17,6 +18,6 @@ function sendSMSMessage(msg, to) {
         .then((message) => console.log(message.sid)).done();
 
 }
-sendSMSMessage("test","16153369743")
-//sendSMSMessage(smsmessageDtl,smsTextNumber)
+//sendSMSMessage("This is a Message","16153369743")
+sendSMSMessage(smsmessageDtl,smsTextNumber)
 module.exports = sendSMSMessage
