0569aad07a674527a357b43dfca8e46358a15204
Jonathan Myer <jonathan.myer@ibm.com>
2022-10-23 16:19:57 -0400

    added template for messaging

--- Node-Notifier/api/notifications.js
+++ Node-Notifier/api/notifications.js
@@ -7,20 +7,41 @@ const authToken = process.env.twilioToken; // Your Auth Token from www.twilio.co
 const client = new twilio(accountSid, authToken);
 
 
-router.get("/notification-email", async(req, res) => {
+router.get("/notification-email", async (req, res) => {
 
 });
 
-router.get("/notification-sms", async(req, res) => {
-    msg = "Test SMS"
-    to = "6153369743"
-    client.messages
+router.get("/notification-sms", async (req, res) => {
+
+    const to = req.query.recipients.split(',')
+    const event = req.query.event_type
+    const event_details = req.query.event_details
+    const event_type_msg_template = {
+        "Clinical-ERAdmission":`A Patient Identified as in your recent care was seen in the ER. Please follow the link for more details ${event_details}`,
+        "Clinical-PtTransfer": "",
+        "Clinical-ICU": "",
+        "Clinical-MedicalTest": "", 
+        "Operations-SystemOutage": ""
+    }
+    
+    to.forEach(async number=>{
+        const message = await client.messages
         .create({
-            body: msg,
-            to: to, // Text this number
+            body: event_type_msg_template[event],
+            to: number, // Text this number
             from: process.env.twilioPhoneNumber
         })
-        .then((message) => console.log(message.sid)).done();
+        .then(message=> {
+            console.log(message.sid)
+            return message.sid
+        }
+        )
+        .catch(err=>{
+            return err
+        })
+    res.send(JSON.stringify(message))
+    })
+
 
 });
 
