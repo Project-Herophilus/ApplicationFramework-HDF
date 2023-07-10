4092f70ae65056ae2c55e5c9223f89a33c6fd6fc
ascott <ascott@clearsense.com>
2023-07-10 12:11:51 -0500

    Initial move from private repo to public different location

---
+++ SpringBoot-DataCrawler/dirwatcher/src/main/java/io/connectedhelth/datacrawler/dirwatcher/DirwatcherApplication.java
@@ -0,0 +1,14 @@
+package io.connectedhelth.datacrawler.dirwatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DirwatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DirwatcherApplication.class, args);
+    }
+
+}
